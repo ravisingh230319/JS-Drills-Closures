ab5dab86896c9748dedff541db4ea17fa943aa65
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-08-17 23:03:20 +0530

    First commit

---
+++ cacheFunction.js
@@ -0,0 +1,18 @@
+export function cacheFunction(cb) {
+  // Should return a funciton that invokes `cb`.
+  // A cache (object) should be kept in closure scope.
+  // The cache should keep track of all arguments have been used to invoke this function.
+  // If the returned function is invoked with arguments that it has already seen
+  // then it should return the cached result and not invoke `cb` again.
+  // `cb` should only ever be invoked once for a given set of arguments.
+  const cache ={};
+  return (par) => {
+    if (!Object.prototype.hasOwnProperty.call(cache,par)) {
+      cache[par] = cb(par);
+      return cache[par];
+    } 
+    else 
+      return cache;
+  }; 
+}
+
